1f20ec5f50745ba1690f13928dd638c3615afa52
Justin Bauer <justinbauer.design@gmail.com>
2021-06-01 17:02:40 +0200

    updates to script.js

--- script.js
+++ script.js
@@ -5,17 +5,21 @@
 ***********************          GitHub: jbNype           **********************
 ***********************     http://justinbauer.design     **********************
 ********************************************************************************/
+
 const buttons = document.querySelectorAll('.num');
+const maths = document.querySelectorAll('.maths');
+const reset = document.querySelector('#reset');
 const displayContainer = document.querySelector('.display-container');
 const display = document.querySelector('#display');
-let displayValue = '';
 
+let displayValue = '';
+let operatorInput;
 
 function add(a, b) {
     return total = a + b;
 }
 
-function substract(a, b) {
+function subtract(a, b) {
     return total = a - b;
 }  
 
@@ -36,6 +40,33 @@ buttons.forEach((button) => {
         displayValue += button.id;
         display.textContent = displayValue;
         displayContainer.appendChild(display);
-    })
+    });
 })
 
+reset.addEventListener('click', () => {
+    displayValue = '';
+    display.textContent = displayValue;
+    displayContainer.appendChild(display);
+})
+
+maths.forEach((math) => {
+    math.addEventListener('click', () => {
+        operatorInput = math.id;
+        switch (operatorInput) {
+            case 'add': add(); break;
+            case 'subtract': subtract(); break;
+            case 'divide': divide(); break;
+            case 'multiply': multiply(); break;    
+        };
+    });
+});
+
+console.log(operator(add, 4, 5));
+
+
+
+// store number when user presses operator
+// save which operator has been selected
+// store second numer user inputs
+// equals runs the operator() function 
+// display solution in displayValue
